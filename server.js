01d34796d1165ef18b672cae35c0d1ef3ee37cb4
Lauren Proctor <102868919+laureneproctor@users.noreply.github.com>
2023-09-11 23:26:48 -0400

    Update server.js
    Code for Bootcamp1

--- server.js
+++ server.js
@@ -1,35 +1,33 @@
 var http = require('http'), 
-    fs = require('fs'), 
+    fs = require('fs'),
     port = 8080;
 
 /* Global variables */
-var listingData, server;
+var listingData, server, myURL;
 
 var requestHandler = function(request, response) {
-  /*Investigate the request object. 
-    You will need to use several of its properties: url and method
-  */
-  //console.log(request);
+  // http.get('http://127.0.0.1:8080/listings', (res) => 
+  // {
+  //   const jsonContent = JSON.stringify(listingData);
+  //   response.write(jsonContent);
 
-  /*
-    Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+  //   let error;
+  //   if (statusCode !== 200) {
+  //     error = new Error('404 - bad gateway error');
+  //   return;
+  //   }
+  // });
+  const jsonContent = JSON.stringify(listingData);
+  response.write(jsonContent);
 
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   
-    HINT: Explore mdn web docs for resources on how to use javascript.
-    Helpful example: if-else structure- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
-
-    */
+  let error;
+  if (request.statusCode !== 200) {
+    error = new Error('404 - bad gateway error');
+  return;
+  }
+  
 };
+
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
   /*
@@ -43,18 +41,28 @@ fs.readFile('listings.json', 'utf8', function(err, data) {
     http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
    */
 
-    //Check for errors
+  //Check for errors
+  if (err) {
+    console.log("404 not found", err);
+    return;
+  }
+    
     /*this resource gives you an idea of the general format err objects and Throwing an existing object.
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw#throwing_an_existing_object
    */
   
 
    //Save the data in the listingData variable already defined
-  
+   listingData = JSON.parse(data);
 
   //Creates the server
-  
-  //Start the server
+  server = http.createServer(requestHandler);
+  //http.get('http://127.0.0.1:8080/listings', http.createServer(requestHandler));
 
+  //Start the server
+  server.listen(port, function() {
+    //once the server is listening, this callback function is executed
+    console.log('Server listening on: http://127.0.0.1:' + port + '/listings');
+  });
 
 });
